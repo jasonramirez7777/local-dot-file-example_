cb6c5c37bb39b3df559b3c95ce5cde3e3cacc401
jasonramirez7777 <jasonramirez7777@gmail.com>
2015-08-21 10:25:33 -0400

    Symlink files that don't currently have symlinks

--- symlink.sh
+++ symlink.sh
@@ -12,12 +12,18 @@ for file in "${Files[@]}"
 do
   if [ -h ~/.$file ]
   then
-    read -p "Delete ~/.$file? [y/n]" -n 1 -r
+    read -p "Delete ~/.$file and create new symlink for $file? [y/n]" -n 1 -r
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]
     then
       rm ~/.$file
       ln -s $PWD/$file ~/.$file
     fi
+  fi
+
+  if [ ! -e ~/.$file ]
+  then
+    echo ~/.$file does not exist. Creating new symlink.
+    ln -s $PWD/$file ~/.$file
   fi
 done
